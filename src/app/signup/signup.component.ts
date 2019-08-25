98f0bd28ac7c04453da625f4d4b0ad1b8c6c67c2
Dominik Garcia Bertapelle <dominikgbertapelle@gmail.com>
2019-08-25 15:11:40 +0200

    Show validation results

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,12 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { SessionService } from '../services/session.service';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.sass']
+  styleUrls: ['./signup.component.sass'],
 })
 export class SignupComponent implements OnInit {
   name: string;
@@ -24,34 +29,36 @@ export class SignupComponent implements OnInit {
   acceptance: boolean;
   signupForm: FormGroup;
   submitted = false;
-  error = false;
+  error: any;
   constructor(
     public userService: UserService,
     private formBuilder: FormBuilder,
     public sessionService: SessionService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.signupForm = this.formBuilder.group({
-        name: ['', Validators.required],
-        lastName: ['', Validators.required],
-        specialty: ['', Validators.required],
-        city: ['', Validators.required],
-        state: ['', Validators.required],
-        address: ['', Validators.required],
-        telephone: ['', [Validators.required, Validators.pattern('[0-9]{7,10}')]],
-        cellphone: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.minLength(6)]],
-        professionalC: ['', Validators.required]
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      specialty: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      address: ['', Validators.required],
+      telephone: ['', [Validators.required, Validators.pattern('[0-9]{7,10}')]],
+      cellphone: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      professionalC: ['', Validators.required],
     });
   }
 
-get f() { return this.signupForm.controls; }
-
+  get f() {
+    return this.signupForm.controls;
+  }
 
   register() {
+    this.error = null;
     this.submitted = true;
     console.log(this.signupForm.invalid, this.signupForm.controls);
     if (this.signupForm.invalid) {
@@ -60,28 +67,27 @@ get f() { return this.signupForm.controls; }
     const data = {
       user: {
         // tslint:disable:no-string-literal
-        first_name: this.signupForm.controls['name'].value ,
+        first_name: this.signupForm.controls['name'].value,
         last_name: this.signupForm.controls['lastName'].value,
         email: this.signupForm.controls['email'].value,
         password: this.signupForm.controls['password'].value,
-        city: this.signupForm.controls['city'].value ,
-        address: this.signupForm.controls['address'].value ,
-        phone: this.signupForm.controls['telephone'].value ,
-        cellphone: this.signupForm.controls['cellphone'].value ,
-        speciality: this.signupForm.controls['specialty'].value ,
+        city: this.signupForm.controls['city'].value,
+        address: this.signupForm.controls['address'].value,
+        phone: this.signupForm.controls['telephone'].value,
+        cellphone: this.signupForm.controls['cellphone'].value,
+        speciality: this.signupForm.controls['specialty'].value,
         professional_license: this.signupForm.controls['professionalC'].value,
-        admin: false
-        }
+        admin: false,
+      },
     };
-    this.userService.signup(data).subscribe((res) => {
+    this.userService.signup(data).subscribe(res => {
       if (!res.error) {
         this.sessionService.setCurrentUser(res);
-      } else {
-        this.error = true;
-      }
-      if (this.sessionService.getCurrentUser()) {
         this.router.navigateByUrl('courses');
+        return;
       }
+
+      this.error = res.error;
     });
   }
 }
