146d06695926f0e47a399be7698d80e792c9fee8
Dominik Garcia Bertapelle <dominikgbertapelle@gmail.com>
2019-11-10 19:47:15 +0100

    Add clusters to menu

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 
 import { FeatherModule } from 'angular-feather';
-import { Edit2, Trash2 } from 'angular-feather/icons';
+import { Edit2, Trash2, Users } from 'angular-feather/icons';
 
 const icons = {
   Edit2,
   Trash2,
+  Users,
 };
 
 @NgModule({
-  imports: [
-    FeatherModule.pick(icons)
-  ],
-  exports: [
-    FeatherModule
-  ]
+  imports: [FeatherModule.pick(icons)],
+  exports: [FeatherModule],
 })
-export class IconsModule { }
+export class IconsModule {}
