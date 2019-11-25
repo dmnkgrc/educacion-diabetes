f52b90290799070e6eed07dcbb6793fd8c729dd9
Dominik Garcia Bertapelle <dominikgbertapelle@gmail.com>
2019-11-25 17:44:34 +0100

    Fix course open bug

--- src/app/courses-index/courses-index.component.ts
+++ src/app/courses-index/courses-index.component.ts
@@ -30,7 +30,7 @@ export class CoursesIndexComponent implements OnInit {
     if (!this.progress[course.id]) {
       return 0;
     }
-    const newIndex = (course.elements.length * this.progress[course.id]) / 100;
+    const newIndex = Math.round((course.elements.length * this.progress[course.id]) / 100);
     if (newIndex >= course.elements.length) {
       return 0;
     }
