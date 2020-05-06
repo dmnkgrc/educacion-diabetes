9b2b71a66d004b2e28fc7cb2add08b0bcc615a9b
Dominik Garcia Bertapelle <dominikgbertapelle@gmail.com>
2020-05-06 16:33:49 +0200

    ci: add deploy step

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/educamed'),
+      dir: require('path').join(__dirname, '../coverage/educacion-diabetes'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
